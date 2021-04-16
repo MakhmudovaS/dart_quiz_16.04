f29e6bf3cd77bcf7285a949efee82f35992fc05f
MakhmudovaS <58090188+MakhmudovaS@users.noreply.github.com>
2021-04-16 10:44:32 +0500

    Create task1.dart

---
+++ task1.dart
@@ -0,0 +1,10 @@
+void main() {
+  String name;
+  int age;
+  name = 'Sevinch';
+  print('Your name is  --> $name');
+  age = 20;
+  print('Your age is --> $age');
+  age = 100 - age;
+  print('$name after --> $age years you will be 100 years old');
+}
